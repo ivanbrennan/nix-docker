cb1f711b9dde42aba65894442b68918becd0d087
ivanbrennan <ivan.brennan@gmail.com>
2019-04-17 09:32:56 -0400

    centos (almost)
    This mostly works. If you build and run the image, you can use the nix
    package manager, e.g.
    
      cd centos
      docker build --tag nix-centos .
    
      docker run -it --rm --name nix-centos nix-centos
      [fnord@dc0eecb459d6 ~]$ nix-env --install --attr man
      installing 'man-db-2.7.5'
      ...
      created 74 symlinks in user environment
    
    However, attempting to update the channel fails due to some ssl ca-cert
    stuff, thus the commented out nix-channel `--add` and `--update`
    commands. Uncommenting them results in the following error during the
    `docker build`:
    
      error: unable to download 'https://nixos.org/channels/nixpkgs-unstable': Problem with the SSL CA cert (path? access rights?) (77)
    
    I think the solution is somewhere in this issue, but haven't gotten it
    sorted out yet: https://github.com/NixOS/nix/issues/2008
    
    Possibly related, attempting to install packages via nix-env directly in
    the Dockerfile hangs. I've commented out the "Install desired packages"
    section of the Dockerfile, so this isn't currently blocking anything,
    but I'm hoping the hanging is related to the above ssl issue. I did not
    experience such behavior in the alpine-based image.

---
+++ centos/nix-install.sh
@@ -0,0 +1,32 @@
+#!/bin/sh
+
+set -eu
+
+NIX_RELEASE=2.1.3
+NIX_HASH=3169d05aa713f6ffa774f001cae133557d3ad72e23d9b6f6ebbddd77b477304f
+DOWNLOAD_URL="https://nixos.org/releases/nix/nix-${NIX_RELEASE}/nix-${NIX_RELEASE}-x86_64-linux.tar.bz2"
+
+sudo groupadd --gid 30000 --system nixbld
+for i in $(seq 1 30)
+do
+    sudo adduser \
+        --system \
+        --home-dir /var/empty \
+        --comment "Nix build user $i" \
+        --uid $((30000 + i)) \
+        --gid nixbld \
+        nixbld$i
+done
+
+set -x
+
+wget -O nix.tar.bz2 $DOWNLOAD_URL
+echo "$NIX_HASH  nix.tar.bz2" | sha256sum -c
+tar xjf nix.tar.bz2
+sh nix-*-x86_64-linux/install
+rm -r nix.tar.bz2 nix-*-x86_64-linux*
+
+bin=/nix/var/nix/profiles/per-user/$USER/profile/bin
+$bin/nix-collect-garbage --delete-old
+$bin/nix-store --optimise
+$bin/nix-store --verify --check-contents
